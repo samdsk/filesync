b9df8dfc4e5402abf774833f8369808acdd2637f
PhantomAgent47 <sam.ds@live.com>
2020-04-17 19:30:51 +0200

    renamed core functions with an appropriate names: remoteUpdateNewFiles - Copies new files to destination folder. remoteUpdateDeletes - Deletes destination folder's non-matching files with source folder. copyNewFile - added a checksum function at the end. deleteFile - changed to Move entire folder/file to System Trash.
    + code has been polished

--- FileSync.java
+++ FileSync.java
@@ -3,12 +3,8 @@ import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
 import java.security.*;
-// import java.nio.file.Paths;
-// import java.nio.file.Path;
-// import java.nio.file.attribute.BasicFileAttributes;
-// import java.nio.file.attribute.FileTime;
-// import java.util.ArrayList;
-// import java.security.DigestInputStream;
+import java.util.Map;
+import java.awt.Desktop;
 
 public class FileSync{
 
@@ -17,101 +13,149 @@ public class FileSync{
     String sourcePath = "M:\\_working_repo\\java\\filesync\\test\\source";
     String destinationPath = "M:\\_working_repo\\java\\filesync\\test\\destination";
 
-/*    if(!source.exists()){
-      System.out.println("Error: Source folder not found!");
-      return;
-    }
+    System.out.println("====================================");
+    System.out.println("Updating destination with new files!");
+    System.out.println("====================================");
 
-    if(!destination.exists()){
-      System.out.println("Error: Destination folder not found!");
-      return;
-    }
-*/
-    fileFinder(sourcePath,destinationPath);
+    remoteUpdateNewFiles(sourcePath,destinationPath);
 
-    //String sourcePath = "./test/source";
-    //Path source = Paths.get(sourcePath);
+    System.out.println("========================");
+    System.out.println("Updating source deletes!");
+    System.out.println("========================");
+
+    remoteUpdateDeletes(sourcePath,destinationPath);
+
 
 
   }
 
-  public static void fileFinder(String sPath, String dPath){
+  public static void remoteUpdateNewFiles(String sPath, String dPath){
     File source = new File(sPath);
     File destination = new File(dPath);
 
     File[] sourceStruct = source.listFiles();
-    //File[] destinationStruct = destination.listFiles();
 
     for(File f : sourceStruct){
+
       String localPathString = f.getAbsolutePath();
       String subPath = localPathString.substring(sPath.length(),localPathString.length());
-
-      System.out.println("Removed source: "+subPath);
-
       String remoteFilePathString = dPath+subPath;
-      System.out.println("Destination remote file: "+remoteFilePathString);
+
+      System.out.println("Working: File: "+localPathString);
+      //System.out.println("Removed source: "+subPath);
+      //System.out.println("Destination remote file: "+remoteFilePathString);
 
       if(f.isFile()){
         try{
-          long fileSize = f.length();
-          System.out.println("OK: File found! size: "+fileSize);
+          //System.out.println("OK: File found!");
           File remoteFile = new File(remoteFilePathString);
           if(remoteFile.exists()){
-            System.out.println("OK: Destination file found!");
-
-            // Path remoteFilePath = Paths.get(remoteFilePathString);
-            // Path localFilePath = Paths.get(localPathString);
-
-            if(timeCheck(localPathString,remoteFilePathString)>0){
-              // BasicFileAttributes remoteFileAttr = attrFinder(remoteFilePath);
-              // BasicFileAttributes localFileAttr = attrFinder(localFilePath);
-
-              System.out.println("Working: Time Check!");
-
-              if(checkSum(localPathString,remoteFilePathString)){
-                System.out.println("Working: Md5 Checksum success! No need to replace!");
-                continue;
-              }else{
-                System.out.println("Working: Md5 Checksum failed! NEED to be replaced!");
-                work(localPathString,remoteFilePathString);
-              }
-
+            if(timeCheck(localPathString,remoteFilePathString)>0 && !checkSum(localPathString,remoteFilePathString)){
+                System.out.println("Working: Copying: Md5 Checksum failed! NEED to be replaced!");
+                copyNewFile(localPathString,remoteFilePathString);
             }
-
+            System.out.println("Working: Skipping: No need to be replaced!");
           }else{
-            System.out.println("Error: Destination file doesnt Exists");
+            System.out.println("Working: Copying: Destination file does not exist!");
+            copyNewFile(localPathString,remoteFilePathString);
           }
         }catch (Exception e){
-          System.out.println(e);
+          e.printStackTrace();
         }
+
         System.out.println("-----------------------------------------------------");
       }else if(f.isDirectory()){
-        System.out.println("OK: Folder found! - "+f.getName());
-        fileFinder(f.getAbsolutePath(),remoteFilePathString);
 
+        System.out.println("OK: Folder found: "+f.getName());
+        try{
+          File remoteFile = new File(remoteFilePathString);
+
+          if(!remoteFile.exists()){
+            Path newDir = Files.createDirectory(Paths.get(remoteFilePathString));
+            System.out.println("Working: New Dir created: "+newDir.toString());
+          }
+
+          remoteUpdateNewFiles(f.getAbsolutePath(),remoteFilePathString);
+
+        }catch(Exception e){
+          e.printStackTrace();
+        }
       }else{
-        System.out.println("Error: neither file nor a folder!");
+        System.out.println("Working: Error: neither file nor a folder!");
         System.out.println("-----------------------------------------------------");
       }
     }
-
 
     return;
   }
 
-  public static void work(String l, String r){
+  public static void remoteUpdateDeletes(String sPath, String dPath){
+    File source = new File(sPath);
+    File destination = new File(dPath);
+    File[] destinationStruct = destination.listFiles();
+
+    for(File f: destinationStruct){
+      String localPathString = f.getAbsolutePath();
+      String subPath = localPathString.substring(dPath.length(),localPathString.length());
+      String remoteFilePathString = sPath+subPath;
+
+      System.out.println("Working: File: "+localPathString);
+
+      if(f.isFile()){
+        try{
+          System.out.println("Working: OK: File found!");
+          File remoteFile = new File(remoteFilePathString);
+          if(!remoteFile.exists()){
+            System.out.println("Working: Warning: Removing: Source file not found.");
+            deleteFile(f);
+          }else{
+            System.out.println("Working: Skipping: Source file exists.");
+          }
+        }catch (Exception e){
+          e.printStackTrace();
+        }
+
+      }else if(f.isDirectory()){
+        System.out.println("Working: OK: Folder found: "+f.getName());
+        try{
+          File remoteFile = new File(remoteFilePathString);
+
+          if(!remoteFile.exists()){
+            System.out.println("Working: Warning: Directory does not exist!");
+            deleteFile(f);
+          }else{
+            remoteUpdateDeletes(remoteFilePathString,f.getAbsolutePath());
+          }
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+
+
+      }else{
+        System.out.println("Working: << Error >>: neither file nor a folder!");
+
+      }
+      System.out.println("-----------------------------------------------------");
+    }
+
+    return;
+  }
+
+  public static void copyNewFile(String l, String r){
     try{
       Path local = Paths.get(l), remote = Paths.get(r);
       if(Files.copy(local,remote,StandardCopyOption.COPY_ATTRIBUTES,StandardCopyOption.REPLACE_EXISTING) != null){
-        System.out.println("Copied with success!");
+
+        if(checkSum(l,r)){
+          System.out.println("Working: Copy New File: Copied with success!");
+        }else{
+          System.out.println("Working: Copy New File: << Error >>!");
+        }
       }
     }catch(Exception e){
       e.printStackTrace();
     }
-
-
   }
-
 
   public static BasicFileAttributes attrFinder(String s){
     BasicFileAttributes attr = null;
@@ -124,27 +168,43 @@ public class FileSync{
     return attr;
   }
 
+  public static void deleteFile(File f){
+    try{
+      Desktop d = Desktop.getDesktop();
+      if(d.isSupported(Desktop.Action.MOVE_TO_TRASH)){
+        if(d.moveToTrash(f)){
+          System.out.println("Working: Delete File: OK: Moved to Recycle Bin successfully!");
+        }else{
+          System.out.println("Working: Delete File: << Error >>: Failed to Move to Recycle Bin!");
+        }
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+
+  }
+
   public static int timeCheck(String l,String r){
     try{
       Path local = Paths.get(l), remote = Paths.get(r);
       FileTime localFileTime = Files.getLastModifiedTime(local);
       FileTime remoteFileTime = Files.getLastModifiedTime(remote);
 
-      System.out.println("Last Modified Time: "+localFileTime.toString());
-      System.out.println("Last Modified Time: "+remoteFileTime.toString());
+      //System.out.println("Time Check: Last Modified Time: "+localFileTime.toString());
+      //System.out.println("Time Check: Last Modified Time: "+remoteFileTime.toString());
 
       if(localFileTime.compareTo(remoteFileTime)>0){
-        System.out.println("Local file is newer!");
+        //System.out.println("Time Check: Local file is newer!");
         return 1;
       }else if(localFileTime.compareTo(remoteFileTime)<0){
-        System.out.println("Remote file is newer!");
+        //System.out.println("Time Check: Remote file is newer!");
         return -1;
       }else{
-        System.out.println("Both same!");
+        //System.out.println("Time Check: Both same!");
         return 0;
       }
     }catch(Exception e){
-      System.out.println("Error: Both same!");
+      System.out.println("Time Check: << Error >>");
       System.out.println(e);
       return -2;
     }
@@ -191,7 +251,5 @@ public class FileSync{
     }
     return result;
   }
-
-
 
 }
